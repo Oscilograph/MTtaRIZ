54fbb9248e0f161261b229c1374038bd6235c14f
Oscilograph <denyahahaha@gmail.com>
2023-04-03 19:55:12 +0300

    Added some verifications to let my code work correctly in more cases

--- Lab2.java
+++ Lab2.java
@@ -8,6 +8,12 @@ public class Lab2 {
 
     // Метод для виводу ялинки на екран
     public static void printTree(int levels) {
+
+        // Перевірка на вірно введені дані в перемінну (перевірка на негативні значення та нульові значення)
+        if (levels <= 0) {
+            System.out.println("Некоректне значення параметру levels.");
+            return;
+        }
         for (int i = 1; i <= levels; i++) {
             for (int j = 1; j <= levels - i; j++) {
                 System.out.print(" ");
@@ -21,6 +27,12 @@ public class Lab2 {
     
     // Метод для створення та виводу двовимірного масиву
     public static void printArray(int rows, int cols) {
+
+        // Перевірка на вірно введені дані в перемінну (перевірка на негативні значення та нульові значення)
+        if (rows <= 0 || cols <= 0) {
+            System.out.println("Некоректне значення параметрів rows або cols.");
+            return;
+        }
         int[][] arr = new int[rows][cols];
         int num = 0;
         for (int i = 0; i < rows; i++) {
